244ff2694cb77c32b5e392feec20223ea5da2d8d
devtahajs <tahaali.css@gmail.com>
2023-07-05 12:44:44 +0530

    name change

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Balance from "../../Components/HomeComps/Balance";
-import Recents from "../../Components/HomeComps/recents";
-import Addings from "../../Components/HomeComps/addings";
+import Balance from "../../Components/HomeComps/Balance.jsx";
+import Recents from "../../Components/HomeComps/recents.jsx";
+import Addings from "../../Components/HomeComps/Addings.jsx";
 
 const HomePage = () => {
   return (
