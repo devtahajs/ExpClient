ab29658eea2875675074166a1cc716d05bc886d2
devtahajs <tahaali.css@gmail.com>
2023-07-05 23:27:35 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   //changing default port
   server: {
@@ -14,6 +13,10 @@ export default defineConfig({
         target: "https://exp-server-five.vercel.app",
         changeOrigin: true,
       },
+    },
+    build: {
+      base: "/",
+      assetsDir: "public",
     },
   },
 });
