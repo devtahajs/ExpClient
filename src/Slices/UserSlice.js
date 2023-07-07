adf8851693faa8436b3965ffded16446496d7a15
devtahajs <tahaali.css@gmail.com>
2023-07-07 07:25:00 +0530

    dd

--- src/Slices/UserSlice.js
+++ src/Slices/UserSlice.js
@@ -5,7 +5,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
     //Login
     login: builder.mutation({
       query: (data) => ({
-        url: "https://exp-server-mikj.onrender.com/api/users/login/",
+        url: "/api/users/login/",
         method: "POST",
         body: data,
       }),
@@ -21,7 +21,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
     //Logout
     logout: builder.mutation({
       query: () => ({
-        url: "https://exp-server-mikj.onrender.com/api/users/logout",
+        url: "/api/users/logout",
         method: "POST",
       }),
     }),
@@ -80,7 +80,7 @@ export const userApiSlice = apiSlice.injectEndpoints({
     //get All Transactions
     alltransaction: builder.mutation({
       query: (data) => ({
-        url: "https://exp-server-mikj.onrender.com/api/users/alltransaction/",
+        url: "/api/users/alltransaction/",
         method: "GET",
       }),
     }),
